c12644d60558bdcece31af1f8b7d787a1d051272
Florian Margaine <florian@margaine.com>
2012-09-08 09:28:37 -0400

    Compiling

--- src/ssh.cc
+++ src/ssh.cc
@@ -2,30 +2,27 @@
 #include <v8.h>
 
 extern "C" {
-    #include <libssh2.h>
+#include <libssh2.h>
 }
 
-using namespace std;
 using namespace node;
 using namespace v8;
 
-Handle<Value> Connect(const Arguments& arg) {
+Handle<Value> Connect( const Arguments& arg ) {
     HandleScope scope;
-    int rc = libssh2_init(0);
-    string status;
+    int rc = libssh2_init( 0 );
     if ( rc == 0 ) {
-        status = "working";
+        return scope.Close( String::New( "working" ) );
     }
     else {
-        status = "not working";
+        return scope.Close( String::New( "not working" ) );
     }
-    return scope.Close(String::New(status));
 }
 
-void init(Handle<Object> target) {
-    target->Set(String::NewSymbol("connect"),
-        FunctionTemplate::New(Connect)->GetFunction());
+void init( Handle<Object> target ) {
+    target->Set( String::NewSymbol( "connect" ),
+        FunctionTemplate::New( Connect )->GetFunction() );
 }
 
-NODE_MODULE(ssh, init)
+NODE_MODULE( ssh, init )
 
